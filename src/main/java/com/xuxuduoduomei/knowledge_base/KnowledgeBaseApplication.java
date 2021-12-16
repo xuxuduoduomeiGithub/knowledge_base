c19686036ee6d152af2584e389ee98751045311f
xuxuduoduomei <1072045748@qq.com>
2021-12-16 20:19:41 +0800

    2-2 init

---
+++ src/main/java/com/xuxuduoduomei/knowledge_base/KnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xuxuduoduomei.knowledge_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuxuduoduomei/knowledge_base/KnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuxuduoduomei.knowledge_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
